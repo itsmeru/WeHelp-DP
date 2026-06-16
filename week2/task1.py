73123aa3e6cd36ab89302e24728f3df1d77dd67d
itsmeru <cat821016@gmail.com>
2026-06-16 20:20:55 +0800

    feat: week2 hw

--- week2/task1.py
+++ week2/task1.py
@@ -1,43 +1,49 @@
+import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 class Line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def vector(self):
         return (self.p2.x - self.p1.x, self.p2.y - self.p1.y)
-    
+
     def is_parallel(self, other):
         v1 = self.vector()
         v2 = other.vector()
         return v1[0] * v2[1] == v1[1] * v2[0]
-    
+
     def is_perpendicular(self, other):
         v1 = self.vector()
         v2 = other.vector()
         return v1[0] * v2[0] + v1[1] * v2[1] == 0
 
+
 class Circle:
     def __init__(self, center, radius):
         self.center = center
         self.radius = radius
-    
+
     def area(self):
-        return 3.14 * self.radius ** 2
-    
+        return math.pi * self.radius ** 2
+
     def is_intersecting(self, other):
         distance_squared = (self.center.x - other.center.x) ** 2 + (self.center.y - other.center.y) ** 2
         radius_sum_squared = (self.radius + other.radius) ** 2
         return distance_squared <= radius_sum_squared
 
+
 class Polygon:
     def __init__(self, points):
         self.points = points
-    
+
     def perimeter(self):
         perimeter = 0
         n = len(self.points)
@@ -49,24 +55,21 @@ class Polygon:
 
         return perimeter
 
+
 line_a = Line(Point(-6, 1), Point(2, 4))
 line_b = Line(Point(-6, -1), Point(2, 2))
 
-# Are Line A and Line B parallel?
-print(line_a.is_parallel(line_b))
 
-# Are Line C and Line A perpendicular?
+print(f'Are Line A and Line B parallel? {line_a.is_parallel(line_b)}')
+
 line_c = Line(Point(-4, -4), Point(-1, 6))
-print(line_c.is_perpendicular(line_a))
+print(f'Are Line C and Line A perpendicular? {line_c.is_perpendicular(line_a)}')
 
-# Print the area of Circle A.
 circle_a = Circle(Point(6, 3), 2)
-print(circle_a.area())
+print(f'Print the area of Circle A. {circle_a.area()}')
 
-# Do Circle A and Circle B intersect?
 circle_b = Circle(Point(8, 1), 1)
-print(circle_a.is_intersecting(circle_b))
+print(f'Do Circle A and Circle B intersect? {circle_a.is_intersecting(circle_b)}')
 
-# Print the perimeter of Polygon A.
 polygon_a = Polygon([Point(2, 0), Point(5, -1), Point(4, -4), Point(-1, -2)])
-print(polygon_a.perimeter())
+print(f'Print the perimeter of Polygon A. {polygon_a.perimeter()}')
--- week2/task2.py
+++ week2/task2.py
@@ -10,29 +10,30 @@ class Enemy:
     def move(self):
         self.x += self.dx
         self.y += self.dy
-    
+
     def is_alive(self):
         return self.health > 0
- 
+
+
 class Tower:
     def __init__(self, x, y, attack_points, attack_range):
         self.x = x
         self.y = y
         self.attack_points = attack_points
         self.range = attack_range
-    
+
     def attack(self, enemies):
         for enemy in enemies:
             if enemy.is_alive():
                 distance_squared = (self.x - enemy.x) ** 2 + (self.y - enemy.y) ** 2
                 if distance_squared <= self.range ** 2:
                     enemy.health -= self.attack_points
-    
 
 
 class BasicTower(Tower):
     def __init__(self, x, y):
         super().__init__(x, y, attack_points=1, attack_range=2)
+
 
 class AdvancedTower(Tower):
     def __init__(self, x, y):
@@ -43,12 +44,12 @@ enemy1 = Enemy("E1", -10, 2, 2, -1, 10)
 enemy2 = Enemy("E2", -8, 0, 3, 1, 10)
 enemy3 = Enemy("E3", -9, -1, 3, 0, 10)
 
-t1 = BasicTower(-3,2)
-t2 = BasicTower(-1,-2)
-t3 = BasicTower(4,2)
-t4 = BasicTower(7,0)
-a1 = AdvancedTower(1,1)
-a2 = AdvancedTower(4,-3)
+t1 = BasicTower(-3, 2)
+t2 = BasicTower(-1, -2)
+t3 = BasicTower(4, 2)
+t4 = BasicTower(7, 0)
+a1 = AdvancedTower(1, 1)
+a2 = AdvancedTower(4, -3)
 
 
 enemies = [enemy1, enemy2, enemy3]
@@ -58,7 +59,7 @@ for turn in range(10):
     for enemy in enemies:
         if enemy.is_alive():
             enemy.move()
-    
+
     for tower in towers:
         tower.attack(enemies)
 
